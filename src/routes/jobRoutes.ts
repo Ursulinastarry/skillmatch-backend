9899dce2980628cd2fa82e2642f33a4fa0af6626
Ursulinastarry <paulinawambua8@gmail.com>
2025-04-21 12:00:28 +0300

    test

--- src/routes/jobRoutes.ts
+++ src/routes/jobRoutes.ts
@@ -10,6 +10,8 @@ router.get("/",  protect,getAllJobs);
 router.put("/:id", protect,updateJob);
 router.delete("/:id", protect,deleteJob);
 router.get("/:id", protect,getJobsById);
-
+router.get('/jobs/:id', (req, res) => {
+    res.json({ message: 'Test route working', id: req.params.id });
+  });
 
 export default router;
