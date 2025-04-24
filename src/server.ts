937946aee7d535cbc413d0bb1c7cfeccfe7edc35
Ursulinastarry <paulinawambua8@gmail.com>
2025-04-24 11:48:45 +0300

    cors

--- src/server.ts
+++ src/server.ts
@@ -33,7 +33,7 @@ const pool = new Pool({
 });
 
 app.use(cors({
-  origin: "http://localhost:4200",
+  origin: ["http://localhost:4200", "http://skillmatchbucket.s3-website-us-east-1.amazonaws.com"],
   methods: "GET, POST,PUT,PATCH,DELETE",
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
@@ -43,7 +43,6 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
-  console.log("🔥 HEADERS:", req.headers);
   next();
 });
 
