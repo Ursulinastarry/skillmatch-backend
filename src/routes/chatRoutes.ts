65d14bdf81c91534d3e4f2338f21e6bc8e0b33a9
Ursulinastarry <paulinawambua8@gmail.com>
2025-04-24 12:34:18 +0300

    routes

--- src/routes/chatRoutes.ts
+++ src/routes/chatRoutes.ts
@@ -1,9 +1,11 @@
 // routes/chat.route.ts
 import express from "express";
-import { chatWithGemini } from "../controllers/chatController";
+import { chatWithGemini,getCareerRecommendations,getJobApplicantMatches,getSuggestedSkills } from "../controllers/chatController";
 import { RequestHandler } from "express";
 
 const router = express.Router();
 router.post("/chat", chatWithGemini as RequestHandler);
-
+router.get("/career/:userId", getCareerRecommendations as RequestHandler);
+router.get("/matches/:jobId", getJobApplicantMatches as RequestHandler);
+router.get("/suggested-skills/:jobId", getSuggestedSkills as RequestHandler);
 export default router;
